e6436ef057ee720313bdb1abf1f21b4d8998c37c
Alexander VanBeekum <beeker@alexanders-mbp>
2019-03-16 12:46:49 -0500

    Initial Commit

---
+++ HW3Tests/HW3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HW3Tests.swift
+//  HW3Tests
+//
+//  Created by Alexander VanBeekum on 3/16/19.
+//  Copyright © 2019 Alexander VanBeekum. All rights reserved.
+//
+
+import XCTest
+@testable import HW3
+
+class HW3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
